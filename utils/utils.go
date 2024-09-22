d096a2fbcd9f6e4efe4c5f49983af22caa040b20
VinGP <105109338+VinGP@users.noreply.github.com>
2024-09-22 23:59:48 +0300

    fix

--- utils/utils.go
+++ utils/utils.go
@@ -17,7 +17,7 @@ type WeekTypeCash struct {
 var weekTypeCash = &WeekTypeCash{}
 
 func GetWeekType() (string, error) {
-	return "знаменатель", nil
+	return "числитель", nil
 	loc, _ := time.LoadLocation("Europe/Moscow")
 	now := time.Now().In(loc)
 
