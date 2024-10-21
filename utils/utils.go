d573c27efdbb024d1a11b953bdf4b6ec617661fb
VinGP <105109338+VinGP@users.noreply.github.com>
2024-10-21 13:39:27 +0300

    fix GetWeekType

--- utils/utils.go
+++ utils/utils.go
@@ -10,18 +10,27 @@ type WeekTypeCash struct {
 	date     time.Time
 }
 
+var weekTypeCash = &WeekTypeCash{}
 var loc, _ = time.LoadLocation("Europe/Moscow")
 var startDate = time.Date(2024, 9, 2, 0, 0, 0, 0, loc)
 
 func GetWeekType() string {
 
-	date := time.Now().In(loc)
+	now := time.Now().In(loc)
 
-	r := date.Sub(startDate).Hours() / 24
-
-	if int(r/7)%2 == 0 {
-		return "числитель"
-	} else {
-		return "знаменатель"
+	if weekTypeCash.WeekType != "" && weekTypeCash.date.Day() == now.Day() {
+		return weekTypeCash.WeekType
 	}
+
+	r := now.Sub(startDate).Hours() / 24
+
+	var res string
+	if int(r/7)%2 == 0 {
+		res = "числитель"
+	} else {
+		res = "знаменатель"
+	}
+	weekTypeCash.WeekType = res
+	weekTypeCash.date = now
+	return res
 }
